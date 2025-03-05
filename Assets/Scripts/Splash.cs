ceee0b6b8c8ae1182d144973d7058c1d5425197a
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-03-06 05:49:15 +0900

    [feat] Splash 씬 이벤트 function 작업

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public void Booting()
+    {
+        GameManager.Instance.SetState(eState.Main_Menu);
+    }
+}
