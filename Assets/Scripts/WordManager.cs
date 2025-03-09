ecf1afc8e47bd5540e64980b345357f358fbc69f
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-03-10 04:00:53 +0900

    [feat] 단어장을 불러오는 메서드 변경

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using UnityEngine.Networking;
 
 [Serializable]
 public class WordBook
 {
     [SerializeField] public List<Word> wordFile;
 }
-
 
 [Serializable]
 public class Word
@@ -23,10 +23,15 @@ public class Word
 /// </summary>
 public class WordManager : MonoBehaviour
 {
+    public bool isLoading;
+
+    [Header("다운로드 서버 URL")]
+    public string __url;
     public string m_section; // 현재 단어 분류
     [SerializeField] public List<Word> wordList; // 불러온 단어 리스트
     [SerializeField] public string m_english; // 현재 선택된 영단어
     [SerializeField] public string m_korean; // 현재 선택된 영단어의 뜻
+    [SerializeField] public int id; // 현재 선택된 단어의 아이디
 
     // WordManager 인스턴스화 싱글톤 패턴
     private static WordManager _instance;
@@ -68,35 +73,67 @@ public class WordManager : MonoBehaviour
         m_korean = "";
         m_section = "";
     }
-
-    // 단어장에서 한 분류를 클릭하면 해당 분류 단어장을 엶 (단어 목록 보기)
-    public IEnumerator Coroutine_OpenWordMenu(string section)
+ 
+    public IEnumerator DownloadFile(string section)
     {
-        string filepath = "Assets/Words/" + section + ".json";
+        isLoading = true;
 
-        string jsonContents = File.ReadAllText(filepath);
+        // 로컬 저장할 공간
+        string filePath = Path.Combine(Application.persistentDataPath, section + ".json");
 
+        if (File.Exists(filePath)) // 이미 다운받은 적이 있다면 return
+        {
+            Set_WordBooks(filePath);
+            isLoading = false;
+        }
+        else // 파일이 없으면
+        {
+            // 다운로드할 url
+            string url = __url + section + ".json";
+            UnityWebRequest request = UnityWebRequest.Get(url);
+            yield return request.SendWebRequest();
+    
+            // 에러 발생 시 
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                isLoading = false;
+                Debug.LogError("File download error: " + request.error);
+
+                GameManager.Instance.ErrorCode = 100;
+            }
+            else // 정상 작동
+            {
+                // 다운로드된 파일을 로컬에 저장
+                File.WriteAllBytes(filePath, request.downloadHandler.data);
+                Debug.Log("File downloaded to: " + filePath);
+            
+                Set_WordBooks(filePath);
+                isLoading = false;
+            }
+        }
+    }
+
+    // 단어장 화면을 구성함
+    void Set_WordBooks(string path)
+    {
+        // 다운로드한 파일을 열어보기
+        string jsonContents = File.ReadAllText(path);
         WordBook content = JsonUtility.FromJson<WordBook>(jsonContents);
+
         // Debug.Log(jsonContents);
 
         wordList.Clear();
-
         // 단어 개수 불러옴
         int length_words = content.wordFile.Count;
-
         for (int i = 0; i < length_words; i++)
         {
             wordList.Add(null);
         }
-
         for (int i = 0; i < length_words; i++)
         {
             wordList[i] =  content.wordFile[i] as Word;
         }
-
         WordSelect.Instance.MakeWordBook();
-
-        yield return null;
     }
 
     // 사용가능한 퀴즈 목록을 엶
