4acc1139d05a4520a25181b238d0f7f78cd1e5a2
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-03-10 04:00:20 +0900

    [feat] 그린 결과를 보여주는 패널들 작업

---
+++ Assets/Scripts/Drawing/Panel/Draw_Result.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Draw_Result : MonoBehaviour
+{
+    [Header("그림을 보여주는 액자")]
+    public GameObject screen_paint; // 그림을 보여줄 공간
+
+    void OnEnable()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Done);
+        StartCoroutine(GetMyPicture());
+    }
+
+    // AR로 보기
+    public void View_AR()
+    {
+
+    }
+
+    public void Onclick_home()
+    {
+        GameManager.Instance.SetState(eState.Main_Menu);
+    }
+
+    // 이전 화면에서 내가 그린 그림을 가져와 적용함
+    IEnumerator GetMyPicture()
+    {
+        string filepath = Application.persistentDataPath + "/myPainting" + ".png";
+        
+        byte[] byteTexture = System.IO.File.ReadAllBytes(filepath);
+        //Debug.Log(byteTexture.Length);
+
+        Texture2D tex = new Texture2D(2, 2);   
+        tex.LoadImage(byteTexture);
+
+        screen_paint.GetComponent<RawImage>().texture = tex;
+
+        // Destroy(tex);
+
+        yield return null;
+    }
+
+    // 내가 그림을 갤러리에 저장함
+    public void SaveMyPicture()
+    {
+        Texture2D tex = new Texture2D(2,2);
+        tex = screen_paint.GetComponent<RawImage>().texture as Texture2D;
+        StartCoroutine(CaptureScreenshotAndSave(tex));
+    }
+
+    private IEnumerator CaptureScreenshotAndSave(Texture2D tex)
+    {
+        yield return new WaitForEndOfFrame();
+        NativeGallery.Permission permission = NativeGallery.CheckPermission(NativeGallery.PermissionType.Write,NativeGallery.MediaType.Image);
+
+        if (permission == NativeGallery.Permission.Denied)
+        {
+            if (NativeGallery.CanOpenSettings())
+            {
+                NativeGallery.OpenSettings();
+            }
+        }
+
+        string photoName = DateTime.Now.ToString(("yyyy_MM_dd_HH_mm_ss"));
+        string folderName = "POP_Kids_English";
+
+        NativeGallery.SaveImageToGallery(tex, photoName, folderName);
+    }
+}
---
+++ Assets/Scripts/Drawing/Panel/Word_DrawResult.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Word_DrawResult : MonoBehaviour
+{
+    public GameObject btn_next; // 다음으로 버튼
+    public TextMeshProUGUI word; // 정답을 보여주는 단어 박스
+
+    [Header("그림을 보여주는 액자")]
+    public RawImage screen_paint; // 그림을 보여줄 공간
+    public Texture2D m_texture;
+
+    void OnEnable()
+    {
+        SoundManager.Instance.PlaySFX(SFX.Done);
+
+        word.text = WordManager.Instance.m_english + " / " + WordManager.Instance.m_korean;
+        Set_NextButton();
+        StartCoroutine(GetMyPicture());
+    }
+
+    // AR로 보기
+    public void View_AR()
+    {
+
+    }
+
+    // 단어장에서 이 단어가 마지막이 아니어야 다음으로 버튼이 나옴
+    public void Set_NextButton()
+    {
+        int m_id = WordManager.Instance.id;
+        int word_length = WordManager.Instance.wordList.Count;
+
+        if (m_id - 1 >= word_length)
+        {
+            btn_next.SetActive(false);
+        }
+        else
+        {
+            btn_next.SetActive(true);
+        }
+    }  
+
+    // 다음 단어 학습 가능
+    public void Onclick_Next()
+    {
+        int m_id = WordManager.Instance.id;
+        WordManager.Instance.id = m_id + 1;
+        m_id = WordManager.Instance.id;
+
+        WordManager.Instance.m_english = WordManager.Instance.wordList[m_id].english;
+        WordManager.Instance.m_korean = WordManager.Instance.wordList[m_id].korean;
+
+        GameManager.Instance.SetState(eState.Word_Main);
+    }
+
+    // 이전 화면에서 내가 그린 그림을 가져와 적용함
+    IEnumerator GetMyPicture()
+    {
+        string filepath = Application.persistentDataPath + "/" + WordManager.Instance.m_section + "_" + WordManager.Instance.m_english + ".png";
+        
+        byte[] byteTexture = System.IO.File.ReadAllBytes(filepath);
+        if (byteTexture.Length > 0)
+        {
+            Texture2D tex = new Texture2D(2, 2);   
+
+            tex.LoadImage(byteTexture);
+            // tex.Apply();
+
+            m_texture = new Texture2D(2, 2);   
+            m_texture = tex;
+
+            //Destroy(tex);
+        }
+        screen_paint.texture = m_texture; 
+
+        yield return null;
+    }
+
+    // 내가 그림을 갤러리에 저장함
+    public void SaveMyPicture()
+    {
+        StartCoroutine(CaptureScreenshotAndSave());
+    }
+
+    private IEnumerator CaptureScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+        NativeGallery.Permission permission = NativeGallery.CheckPermission(NativeGallery.PermissionType.Write,NativeGallery.MediaType.Image);
+
+        if (permission == NativeGallery.Permission.Denied)
+        {
+            if (NativeGallery.CanOpenSettings())
+            {
+                NativeGallery.OpenSettings();
+            }
+        }
+
+        string photoName = WordManager.Instance.m_section + "_" + WordManager.Instance.m_english;
+        string folderName = "POP_Kids_English";
+
+        NativeGallery.SaveImageToGallery(m_texture, photoName, folderName);
+    }
+}
