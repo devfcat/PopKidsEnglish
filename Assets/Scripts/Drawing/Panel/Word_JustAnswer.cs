f150d57a5ce7e5e75b919888d064b4a812eb6440
Devfcat <137423690+devfcat@users.noreply.github.com>
2025-03-10 04:14:36 +0900

    [feat] 그냥 정답보기 패널 작업

--- Assets/Scripts/Drawing/Panel/Word_JustAnswer.cs
+++ Assets/Scripts/Drawing/Panel/Word_JustAnswer.cs
@@ -6,8 +6,39 @@ using TMPro;
 public class Word_JustAnswer : MonoBehaviour
 {
     public TextMeshProUGUI word;
+    public GameObject btn_next;
+
     void OnEnable()
     {
         word.text = WordManager.Instance.m_english + " / " + WordManager.Instance.m_korean;
+    }
+
+    // 단어장에서 이 단어가 마지막이 아니어야 다음으로 버튼이 나옴
+    public void Set_NextButton()
+    {
+        int m_id = WordManager.Instance.id;
+        int word_length = WordManager.Instance.wordList.Count;
+
+        if (m_id - 1 >= word_length)
+        {
+            btn_next.SetActive(false);
+        }
+        else
+        {
+            btn_next.SetActive(true);
+        }
+    }  
+
+    // 다음 단어 학습 가능
+    public void Onclick_Next()
+    {
+        int m_id = WordManager.Instance.id;
+        WordManager.Instance.id = m_id + 1;
+        m_id = WordManager.Instance.id;
+
+        WordManager.Instance.m_english = WordManager.Instance.wordList[m_id].english;
+        WordManager.Instance.m_korean = WordManager.Instance.wordList[m_id].korean;
+
+        GameManager.Instance.SetState(eState.Word_Main);
     }
 }
