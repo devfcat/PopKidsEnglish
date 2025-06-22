1930d4bcfb4ff4e368f34c3e24af4042d1bff94e
devfcat <devfcat@gmail.com>
2025-06-22 18:40:41 +0900

    [feat] 선택된 단어의 분류를 wordManager에 저장

--- Assets/Scripts/Main/Panels/LearnSelect.cs
+++ Assets/Scripts/Main/Panels/LearnSelect.cs
@@ -7,6 +7,7 @@ public class LearnSelect : MonoBehaviour
     public void OnClick(string section)
     {
         LearnSelect_Manager.Instance.m_selected_section = section;
+        WordManager.Instance.m_section = LearnSelect_Manager.Instance.m_selected_section;
         GameManager.Instance.SetState(eState.MyDrawing_Menu);
     }
 }
