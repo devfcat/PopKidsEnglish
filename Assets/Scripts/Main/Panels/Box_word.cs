2e2ab7f42f504849754ef4cd019a8db1803bab4f
devfcat <devfcat@gmail.com>
2025-06-22 18:40:19 +0900

    [feat] WordManager에 선택한 단어의 분류 저장

--- Assets/Scripts/Main/Panels/Box_word.cs
+++ Assets/Scripts/Main/Panels/Box_word.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 단어장 목록에서 선택한 단어를 WordManager에 저장하고 화면 전환
+/// </summary>
 public class Box_word : MonoBehaviour
 {
     public int id; // 단어장에서 몇 번째인가
@@ -13,7 +16,11 @@ public class Box_word : MonoBehaviour
         WordManager.Instance.m_english = WordManager.Instance.wordList[id].english;
         WordManager.Instance.m_korean = WordManager.Instance.wordList[id].korean;
 
-        if (GameManager.Instance.m_state == eState.MyDrawing_Menu) GameManager.Instance.SetState(eState.MyDrawing_View);
+        if (GameManager.Instance.m_state == eState.MyDrawing_Menu) 
+        {
+            WordManager.Instance.m_section = LearnSelect_Manager.Instance.m_selected_section;
+            GameManager.Instance.SetState(eState.MyDrawing_View);
+        }
         else GameManager.Instance.SetState(eState.Word_Main);
     }
 }
